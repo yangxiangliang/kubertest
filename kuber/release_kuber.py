cd1c93c45cfd71857e11edb1a46942572fbffa88
Sean Yang <sean.yang@sojern.com>
2015-10-15 21:53:07 -0700

    sean yang test

--- kuber/release_kuber.py
+++ kuber/release_kuber.py
@@ -11,7 +11,8 @@ import os
 
 handler = logging.StreamHandler(sys.stdout)
 handler.setFormatter(logging.Formatter(
-    '%(asctime)s - %(levelname)s - %(filename)s:%(funcName)s:%(lineno)d - %(message)s', '%Y-%m-%d-%H:%M:%S'))
+    '%(asctime)s - %(levelname)s - %(filename)s:%(funcName)s:%(lineno)d - %(message)s', '%Y-%m-%d-%H:%M:%S'  # NOQA
+))
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger().addHandler(handler)
 log = logging.getLogger(__name__)
